a33780f1b168cfb2415aaea9de0b93339a61a260
jdprada1760 <jd.prada1760@uniandes.edu.co>
2017-07-31 19:08:28 +0200

    Reorganizing code

--- Code/shape.py
+++ Code/shape.py
@@ -5,6 +5,11 @@ from arepo import *
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
+import yt
+from yt.units import parsec, Msun
+
+DMass = 0
+
 
 # Loads given snapshot of the simulation given the needed parameters
 # lvl -> The level of the simulation
@@ -13,19 +18,24 @@ from matplotlib.ticker import FormatStrFormatter
 # subSnap -> Name of the file where group and subgroup information is stored
 # return pos -> The centered position of the main structure particles (filter substructures)
 # return rad -> The virial radius 
-def loadSim(lvl,halo,snap,subSnap):
+def loadSim(lvl,halo,snapnum):
+    global DMass
+
     # Path of the simulation
     path = "/hits/universe/GigaGalaxy/"+lvl+"/"+halo+"/output" 
 
     # Loads groups to select the principal halo
-    subSn = Subfind(path+subSnap)
+    subSn = Subfind(path,snapnum)
 
     # filterr = [Halo(subSn, halo = 0)]
     # filterr = [Halo(subSn, halo = 0, subhalo = subSn.GroupFirstSub[0])]
 
     # Loads the simulation
     #sn = Snapshot(path+snap, parttype=[1], filter=filterr, combineFiles=True, verbose=True)
-    sn = Snapshot(path+snap, parttype=[1], combineFiles=True, verbose=True)
+    sn = Snapshot(path,snapnum, parttype=[1], combineFiles=True, verbose=True)
+    
+    # DM Mass
+    DMass = sn.MassTable[1]
 
     # r the radius of the halo (R_mean200)*1000 to obtain it in kpc
     rad = 1000.*subSn.group.Group_R_TopHat200[0]
@@ -126,64 +136,6 @@ def recalculate_Semiaxes(a,b,c,pos,epsilon=1e-6):
         #print(ratio,a,b,c)
     return a,b,c,pos
 
-def plotHalo(filename,a,b,c,pos,lvl,halo):
-    rad = (a*b*c)**(1./3.)
-    # Draws ellipse
-    from matplotlib.patches import Ellipse
-    elli = Ellipse(xy=[0,0],width = 2*a, height = 2*b, fill = False)  
-    # Filter box
-    ind = np.where((abs(pos[:,0])< 1.5*a) & (abs(pos[:,1])< 1.5*a)& (abs(pos[:,2])< 1.5*a))[0]
-    npos = pos[ind,:]
-    # Plot
-    fig = plt.figure(0)
-    ax = fig.add_subplot(111, aspect='equal')
-    ax.add_artist(elli)
-    markersize = 0.15*(rvir/rad)
-    '''
-    if(Ni is None):
-        alpha = 0.2
-        markersize = 0.5
-    '''
-    #alpha = (10.0**(-1.0*len(npos)/Ni))
-    alpha = 0.1
-    label = "r="+'{:.2e}'.format(float(rad))
-    #label += "\n b/a="+'{:.2e}'.format(float(b/a))
-    #label += "\n c/a="+'{:.2e}'.format(float(c/a))
-    #label += "\n c="+'{:.2e}'.format(float(c))
-    ax.plot([0],[0], marker = 'o')
-    ax.plot(npos[:,0], npos[:,1], marker = '.', linewidth = 0, alpha = alpha, markersize = markersize, label = label)
-    ax.set_xlim(-1.5*a,1.5*a)
-    ax.set_ylim(-1.5*a,1.5*a)
-    ax.set_title(filename.split('.')[0])
-    ax.legend()
-    plt.savefig("../Plots/"+lvl+"/"+halo+"/"+filename)
-    plt.close()
-
-# Generates random elliptical ditribution, gaussian in radius given semiaxes
-# a,b,c semiaxes
-def gen_ellipse(a,b,c, n_points):
-    pos = []
-    x,y,z = 0,0,0
-    pos.append([x,y,z])
-    while len(pos) < n_points :
-        x_new = np.random.normal(loc = x, scale = a/4.)
-        y_new = np.random.normal(loc = y, scale = b/4.)
-        z_new = np.random.normal(loc = z, scale = c/4.)
-        alpha = dist(x_new,y_new,z_new,a,b,c)/dist(x,y,z,a,b,c)
-        if alpha >= 1:
-            x,y,z = x_new,y_new,z_new
-        else:
-            if np.random.random()<alpha:
-                x,y,z = x_new,y_new,z_new
-        pos.append([x,y,z])
-    return np.array(pos)
-
-
-# Elliptical gaussian distribution
-def dist(x,y,z,a,b,c):
-    return (2*np.pi)**(-3./2.)*a*b*c*np.exp(-((x/a)**2+(y/b)**2+(z/c)**2))
-
-
 # Plots many graphics of ratios (must be between 0 and 1)
 # yvals -> Fields to plot on x axis (may be more than one array)
 # xvals -> Only one array for x axis
@@ -202,94 +154,130 @@ def ratiosPlots(xvals, yvals, ylabel, rvir):
      axs[-1].set_xlabel("log(R(kpc/h))")
      return fig,axs
 
-'''
-Ni = None
-# Generates ellipse
-a,b,c = 4.,3.,2.
-pos = gen_ellipse(a,b,c,100000)-np.array([2,5,3])
-#np.array(np.random.random(3)*np.array([a,b,c])*0.5)
-# Random rotation
-theta = np.random.random()*np.pi 
-Rot = np.array([[np.cos(theta),-np.sin(theta),0],[np.sin(theta),np.cos(theta),0],[0,0,1]])
-pos = Rot.dot(pos.T).T
-theta = np.random.random()*np.pi 
-Rot = np.array([[np.cos(theta),0,-np.sin(theta)],[0,1,0],[np.sin(theta),0,np.cos(theta)]])
-pos = Rot.dot(pos.T).T
-rad = 5
-a,b,c,pos = recalculate_Semiaxes(rad,rad,rad,pos,n_it = 50)
-plotHalo("noCM_Ellipse_5_(2,5,3).png",a,b,c,pos)
-print(b/a,c/a,'|__|',3./4.,2./4.)
-print(np.abs((b/a)-(3./4.))/(3./4.),np.abs((c/a)-(2./4.))/(2./4.))
+# Plots Halo with YT
+def plotHalo(filename,a,b,c,pos,lvl,halo):
+    rad = (a*b*c)**(1./3.)
+    # Filter box
+    ind = np.where((abs(pos[:,0])< 1.5*a) & (abs(pos[:,1])< 1.5*a)& (abs(pos[:,2])< 2*a))[0]
+    npos = pos[ind,:]
+    ppx,ppy,ppz = npos.T
 
-# Plot
-plt.plot(pos[:,0],pos[:,1], marker = '.', linewidth = 0, alpha = 0.1, markersize =0.5)
-plt.xlim(-4*a,4*a)
-plt.ylim(-4*b,4*b)
-#plt.zlim(-1.5*c,1.5*c)
-plt.savefig("Processed_Ellipse.png")
-plt.close()
-'''
-'''
-lvl = 'level5_Durham'
-#lvl = 'level5' 
-for j in range(25,26):
-    print("-------------------------------------------------")
-    print(j)
-    halo = 'halo_'+str(j)
-    snap = "/snapshot_255.hdf5"
-    subSnap = "/fof_subhalo_tab_255.hdf5"
-    pos,rvir = loadSim(lvl,halo,snap,subSnap)
-    rvir
-    print("Radius of the simulation:  "+str(rvir))
-    # Logspace for radius
-    logdelta = 3.0/50.0
-    logr_ini = -1
-    # Keeps semiaxes
-    semmiaxes = []
-    for i in range(1000):
-        x_rad = 10.0**(logr_ini+i*logdelta)
-        #x_rad =340.0
-        a,b,c,posx = recalculate_Semiaxes(x_rad,x_rad,x_rad,pos)
-        if a > 0:
-            semmiaxes.append([a,b,c])
-            if i%10 == 0:
-                title = lvl+"_"+halo+"_"+str(i//10)+".png"
-                plotHalo(title,a,b,c,posx,lvl,halo)
-        rad = (abs(a*b*c))**(1./3.)
-        #print("________________________________________________________________")
-        print(x_rad,rad)
-        if( x_rad > 2.0*rvir ):
-            break
-        # Uses last semiaxes to simplify further calculations by rescaling
-        #scale = xrad[i+1]/xrad[i]
-        #a,b,c = scale*a,scale*b,scale*c
-    semmiaxes = np.array(semmiaxes)
-    np.savetxt("../Plots/"+lvl+"/"+halo+"/"+"abc_"+lvl+"_"+halo+".txt",semmiaxes, delimiter = ',')
-    if(len(semmiaxes) != 0):
-        a,b,c = semmiaxes.T
-        yvals = np.array([b/a,c/a,c/b])
-        ylabel = ['b/a','c/a','c/b']
-        xvals = (a*b*c)**(1./3.)
-        fig,axs = ratiosPlots(xvals, yvals, ylabel, rvir)  
-        plt.savefig("../Plots/"+lvl+"/"+halo+"/"+"Axial_ratios_vs_R__"+lvl+"_"+halo+".png")
-        plt.close()
-'''
-from matplotlib.backends.backend_pdf import PdfPages
-import matplotlib.pyplot as plt
-lvl = 'level5_Durham'
-with PdfPages('../Plots/'+lvl+'/AxialRatios.pdf') as pdf:
-    for j in range(1,31):
+    # Creates data to work with in YT
+    data = {'particle_position_x': ppx,
+            'particle_position_y': ppy,
+            'particle_position_z': ppz,
+            'particle_mass': DMass*np.ones(len(npos))}
+
+    bbox = 1.1*np.array([[min(ppx), max(ppx)], [min(ppy), max(ppy)], [min(ppz), max(ppz)]])
+    ds = yt.load_particles(data, length_unit=1000*parsec, mass_unit=1e10*Msun, n_ref=64, bbox=bbox)
+    ad = ds.all_data()
+
+    # This is generated with "cloud-in-cell" interpolation.
+    #cic_density = "deposit", "all_cic"
+    # Neares Neighbor
+    nn_density = "deposit", "all_density"
+    #nn_deposited_mass = "deposit", "all_mass"
+    #particle_count_per_cell = "deposit", "all_count"
+
+    # Choose a center for the render.
+    c = [0, 0, 0]
+
+    p = yt.ProjectionPlot(ds, 'z', nn_density, center=c, weight_field=nn_density, width=(3*a, 'kpc'))
+    #p = yt.ParticlePlot(ds, 'particle_position_x','particle_position_y', weight_field=nn_density, width=(2*rad,2*rad))
+
+    # Title
+    p.annotate_title(filename.split('.')[0])
+    # Changes CMap
+    p.set_cmap(nn_density,'inferno')
+    # Draws density contour
+    p.annotate_contour(nn_density, ncont = 10, take_log = True )
+    # Plots center of the halo (potential) 
+    p.annotate_marker((0,0), coord_system='plot',plot_args={'color':'blue','s':500})
+
+    p.save("tmp.png")
+    #p.display()
+
+    ##############################################################################################
+
+    # Gets matplotlib figure,axes
+    mpl = p.plots[nn_density]
+    mpl.axes.plot([0],[0], marker = 'o')
+    # Draws ellipse
+    from matplotlib.patches import Ellipse
+    elli = Ellipse(xy=[0,0],width = 2*a, height = 2*b, fill = False, linewidth = 2.5, label = "r="+'{:.2e}'.format(float(rad)))  
+    # Filter box
+    mpl.axes.add_artist(elli)
+    leg  = mpl.axes.legend(facecolor='w')
+    #text = leg.get_texts()
+    #plt.setp(text, color = 'w')
+    #mpl.figure.savefig("Otro.png")
+
+    #############################################################################################
+
+
+    mpl.figure.savefig("../Plots/"+lvl+"/"+halo+"/"+filename)
+
+    plt.close()
+
+def main():
+    #lvl = 'level5'
+    lvl = 'level4_MHD'
+    #lvl = 'level5_Durham' 
+    halonums = [20]
+    #halonums = range(1,11)
+    #halonums = ['halo16','halo16_MHD','halo24','halo24_MHD','halo28', 'halo6_MHD','halo9','halo9_MHD']
+    for j in halonums:
+        print("--------------------------------------------------------------------------")
+        print(j)
         halo = 'halo_'+str(j)
-        print(halo)
-        path = "../Plots/"+lvl+"/"+halo+"/"+"abc_"+lvl+"_"+halo+".txt"
-        a,b,c = np.loadtxt(path,delimiter = ',').T
-        snap = "/snapshot_255.hdf5"
-        subSnap = "/fof_subhalo_tab_255.hdf5"
-        pos,rvir = loadSim(lvl,halo,snap,subSnap)
-        yvals = np.array([b/a,c/a,(a**2-b**2)/(a**2-c**2)])
-        ylabel = ['b/a','c/a','T']
-        xvals = (a*b*c)**(1./3.)
-        fig,axs = ratiosPlots(xvals, yvals, ylabel, rvir)
-        axs[0].set_title(halo)
-        pdf.savefig(fig)
-        plt.close()
+        #halo = j
+        #snap = "/snapdir_127/"
+        #subSnap = "/groups_127/"
+        snapnum = 127
+        #snapnum = 63
+        #snapnum = 255
+        pos,rvir = loadSim(lvl,halo,snapnum)
+        rvir
+        print("Radius of the simulation:  "+str(rvir))
+        # Logspace for radius
+        logdelta = 3.0/50.0
+        logr_ini = -1
+        # Keeps semiaxes
+        semmiaxes = []
+        x_rad = 10.0**(logr_ini)
+        a,b,c = x_rad,x_rad,x_rad 
+        for i in range(1000):
+            a,b,c,posx = recalculate_Semiaxes(a,b,c,pos)
+            x_rad = 10.0**(logr_ini+(i+1)*logdelta)
+            if a > 0:
+                semmiaxes.append([a,b,c])
+                # Uses last result to improve convergence
+                b,c = (x_rad/a)*b,(x_rad/a)*c
+                a = x_rad
+                pos = posx
+                m = 1
+                if i%m == 0:
+                    title = lvl+"_"+halo+"_"+str(i//m)+".png"
+                    #plotHalo(title,a,b,c,posx,lvl,halo)
+            else:
+                a,b,c = x_rad,x_rad,x_rad
+
+            rad = (abs(a*b*c))**(1./3.)
+            #print("________________________________________________________________")
+            print(x_rad,rad)
+            if( x_rad > 2.0*rvir ):
+                break
+        semmiaxes = np.array(semmiaxes)
+        np.savetxt("../Plots/"+lvl+"/"+halo+"/"+"abc_"+lvl+"_"+halo+".txt",semmiaxes, delimiter = ',')
+    '''
+        if(len(semmiaxes) != 0):
+            a,b,c = semmiaxes.T
+            yvals = np.array([b/a,c/a,c/b])
+            ylabel = ['b/a','c/a','c/b']
+            xvals = (a*b*c)**(1./3.)
+            fig,axs = ratiosPlots(xvals, yvals, ylabel, rvir)  
+            plt.savefig("../Plots/"+lvl+"/"+halo+"/"+"Axial_ratios_vs_R__"+lvl+"_"+halo+".png")
+            plt.close()
+    '''
+
+
