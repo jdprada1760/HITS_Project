ea878542fa1e2067f073e03bd9407aea49026876
Jesus Prada <jd.prada1760@uniandes.edu.co>
2017-12-13 16:31:39 +0100

    Code updates that had not been submited

---
+++ Code/Convergence_Analysis.py
@@ -0,0 +1,127 @@
+import matplotlib
+matplotlib.use('Agg')
+
+# Python
+from arepo import *
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
+from matplotlib.backends.backend_pdf import PdfPages
+
+# Simiulation specs
+lvl3 = "level3_MHD"
+lvl4 = "level4_MHD"
+halonums = np.array([6,16,21,23,24,27])
+#halonums = [24]
+with PdfPages("../Plots/Convergence_lvl34.pdf") as pdf:
+    for j in halonums:
+        # Minimum array at which there is enough particles for the shape to converge (among all graphics)
+        #mini = 121
+        mini = 148
+        # Halo name        
+        halo = "halo_"+str(j)        
+    
+        # Paths to files
+        path3 = "../Plots/"+lvl3+"/"+halo+"/"+"abc_"+lvl3+"_"+halo+".txt"
+        path4 = "../Plots/"+lvl4+"/"+halo+"/"+"abc_"+lvl4+"_"+halo+".txt"
+    
+        # Semiaxes
+        arr3 = np.loadtxt(path3, delimiter = ",")[-mini:]
+        arr4 = np.loadtxt(path4, delimiter = ",")[-mini:]
+        rvir = arr3[-1][0]
+        a3,b3,c3 = arr3[:-1].T
+        a4,b4,c4 = arr4[:-1].T
+
+        # Axial ratios and radii
+        yvals3 = np.array([b3/a3,c3/a3,c3/b3])
+        xvals3 = (a3*b3*c3)**(1./3.)
+        yvals4 = np.array([b4/a4,c4/a4,c4/b4])
+        xvals4 = (a4*b4*c4)**(1./3.)
+         
+        # The difference in axis ratios (we are omitting differences in a)
+        diff = []
+        count = 0.0
+
+        # Finds the closest radius in level3 for each in level4
+        for i4 in range(len(xvals4)):            
+            
+            # The index of closest radius in level3
+            i3 = np.argmin(abs(xvals3-xvals4[i4]))
+
+            # Appends the difference in axis ratios
+            diff.append(abs((yvals3.T[i3]-yvals4.T[i4])/yvals3.T[i3]))
+
+            if( abs(xvals3[i3] - xvals4[i4])/xvals3[i3] > 0.03 ):
+                #print(halo+"____r= "+str(xvals3[i3])+"____delta= "+str(abs(xvals3[i3] - xvals4[i4])/xvals3[i3]))
+                count += 1
+        print(halo+"____Percent of points above 1% error in radius: " +str(count/len(xvals4)))
+        diff = np.array(diff)
+        
+        # keeps track of standard deviation and mean
+        #mini = 121
+        xrand = np.zeros(mini)
+        yrand = []
+
+        # Graphics
+        ylabel = ['b/a','c/a','c/b']
+        fig, axs = plt.subplots(figsize=(10,10),nrows=len(yvals3))
+        for ax,yval3,yval4,ylab,delta,indi in zip(axs,yvals3,yvals4,ylabel,diff.T,range(3)):
+            
+            yrand = []
+            # Random sampling graphics
+            for randn in range(10):
+                path = "../Plots/"+lvl3+"/"+halo+"/rand_sample/"+str(randn)+".txt"
+                arr = np.loadtxt(path, delimiter = ",")[-mini:]
+                a,b,c = arr[:-1].T
+                xvals = (a*b*c)**(1./3.)
+                yvals = np.array([b/a,c/a,c/b])
+                ax.plot(xvals,yvals[indi], c = 'grey', linewidth=0.1, zorder = 1)
+                
+                # Statistics
+                yrand.append(yvals[indi,-mini:])
+                #print len(yvals[-mini:])
+                if randn == 9:
+                    xrand = xvals[-mini:]
+            
+            yrand = np.array(yrand)
+            #print(xrand[-mini:])
+            
+            # Std and mean value
+            #ax.plot(xrand[-mini:], np.mean(yrand,axis = 0)[-mini:], c = 'black', linewidth=0.2)
+            
+            yerr = np.zeros(len(xvals3))
+            for l in range(len(yrand[0])):
+                yerr[-(l+1)] = np.std(yrand[:,-(l+1)])
+            
+            #print yerr
+            # Plots level3, level4 and their difference
+            ax.errorbar(xvals3,yval3,yerr=0*yerr, c = 'blue', label = str(lvl3), linewidth=1.2, zorder = 2)
+            
+            ax.fill_between(xvals3, yval3 - 2*yerr,  
+                 yval3 + 2*yerr, color="blue", alpha = 0.3, zorder = 1) 
+                 
+            ax.plot(xvals4,yval4, c = 'magenta', label = str(lvl4), linewidth=1.2, zorder = 10)
+            ax.plot(xvals4,delta, c = 'g', label = "Difference", linewidth=1)
+            ax.axhline(0.05, linewidth = 0.1)
+            ax.plot([rvir,rvir],[0,1])
+            ax.set_xscale('log')
+            
+            # Plotting ratios
+            ax.set_ylim(0,1)
+            
+            # Axs specs
+            ax.set_xlim(0.5,xvals3[-1]+30)
+            ax.xaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+            ax.set_ylabel(ylab)
+            ax.legend()
+            
+        # Axs specs    
+        axs[-1].set_xlabel("log(R(kpc/h))")
+        axs[0].set_title(halo+"  Rvir="+str(rvir)+"Kpc" )        
+        pdf.savefig(fig)
+        plt.close()
+
+
+
+        
+        
--- Code/get_Axes.py
+++ Code/get_Axes.py
@@ -1,5 +1,6 @@
 import matplotlib
 matplotlib.use('Agg')
+import os
 
 # Python
 from arepo import *
@@ -9,6 +10,7 @@ from matplotlib.ticker import FormatStrFormatter
 import yt
 from yt.units import parsec, Msun
 from Py_Libs.shape import *
+import os as os
 
 # C imports
 import ctypes as ct
@@ -16,9 +18,9 @@ inertia = ct.cdll.LoadLibrary('./C_Libs/inertia.so')
 
 # Simiulation specs
 #lvl = 'level5'
-lvl = 'level4_MHD'
+#lvl = 'level4_MHD'
 #lvl = 'level5_Durham' 
-halonums = [20]
+#halonums = [27]
 #halonums = range(1,11)
 #halonums = ['halo16','halo16_MHD','halo24','halo24_MHD','halo28', 'halo6_MHD','halo9','halo9_MHD']
 
@@ -35,56 +37,115 @@ print(eigenvecs)
 '''
 
 #lvl = 'level5'
-lvl = 'level4_MHD'
+#lvl = 'level3_MHD'
+lvl = 'level3_DM'
 #lvl = 'level5_Durham' 
-#halonums = [20]
-halonums = range(1,31)
-#halonums = ['halo16','halo16_MHD','halo24','halo24_MHD','halo28', 'halo6_MHD','halo9','halo9_MHD']
+#lvl = 'level2'
+halonums = [21,23,24,27]
+#halonums = range(1,31)
+#halonums = [24]
 for j in halonums:
     print("--------------------------------------------------------------------------")
     print(j)
+    #halo = "halo" + str(j) +"_MHD"        
     halo = 'halo_'+str(j)
-    #halo = j
-    #snap = "/snapdir_127/"
-    #subSnap = "/groups_127/"
-    snapnum = 127
-    #snapnum = 63
-    #snapnum = 255
-    pos,rvir = loadSim(lvl,halo,snapnum)
-    pos = np.array(pos,dtype = np.float)
-    print("Radius of the simulation:  "+str(rvir))
-    # Logspace for radius
-    logdelta = 3.0/50.0
-    logr_ini = -1
-    # Keeps semiaxes
-    semmiaxes = []
-    x_rad = 10.0**(logr_ini)
-    axes = np.ones(3,dtype = np.float)*x_rad
-    vecs = np.identity(3,dtype = np.float)
-    for i in range(1000):
-        inertia.get_shape(ct.c_void_p(pos.ctypes.data),ct.c_int(len(pos)), ct.c_void_p(vecs.ctypes.data), ct.c_void_p(axes.ctypes.data))
-        x_rad = 10.0**(logr_ini+(i+1)*logdelta)
-        if axes[0] > 0:
-            semmiaxes.append(np.copy(axes))
-            # Uses last result to improve convergence
-            axes[1:] = (x_rad/axes[0])*axes[1:]
-            axes[0] = x_rad
-            #m = 1
-            #if i%m == 0:
-                #title = lvl+"_"+halo+"_"+str(i//m)+".png"
-                #plotHalo(title,a,b,c,posx,lvl,halo)
-        else:
-            axes = np.ones(3,dtype = np.float)*x_rad
-            vecs = np.identity(3,dtype = np.float)
+    
+    #snapnum = 17
+    snapnum = 119
+    #snapnum = 59    
+    # This boolean determines the stop of the snapshot calculations
+    boole = True
+    
+    while(boole):
+        
+        # Loads particles positions, virial radius and other important info
+        pos,rvir,info = loadSim(lvl,halo,snapnum)
+        pos = np.array(pos,dtype = np.float)
+        
+        # Stops if 
+        if (rvir < 20) : 
+            boole = False
+        
+        print("---------------------------------------------------------------------------------")
+        print("----------------------------------------------------------Halo:  "+halo)
+        print("----------------------------------------------------------Snap:  "+str(snapnum))
+        print("----------------------------------------------------------Rvir:  "+str(rvir))
+        print("-------------------------------------------------------NUMPART:  " +str(len(pos)))
+        print("---------------------------------------------------------------------------------")
+        # Logspace for radius
+        logdelta = 2.0/50.0
+        #logdelta = 1.0/50.0
+        logr_ini = 0.5
+        
+        # Keeps semiaxes
+        semmiaxes = []
+        # Initial radius, axes and inertia
+        x_rad = 10.0**(logr_ini)
+        axes = np.ones(3,dtype = np.float)*x_rad
+        vecs = np.identity(3,dtype = np.float)
+        
+        for i in range(1000):
+        
+            print("_______________RAD    : " +str(x_rad))
+         
+            # Ensures that we obtain an output radius near the corresponding radius
+            rad = 0
+            l = 0
+            while True:
+                
+                inertia.get_shape(ct.c_void_p(pos.ctypes.data),ct.c_int(len(pos)), ct.c_void_p(vecs.ctypes.data), ct.c_void_p(axes.ctypes.data))
+                rad = (abs(axes[0]*axes[1]*axes[2]))**(1./3.)
+                # Advance
+                if axes[0] < 0:
+                    break
+                elif abs(rad-x_rad)/x_rad > 0.01 :
+                    axes = (axes[0]+1.5*(x_rad-rad))*axes/axes[0]
+                    #axes = np.ones(3,dtype = np.float)*(axes[0]+1.*(x_rad-rad))
+                    #vecs = np.identity(3,dtype = np.float)  
+                else:
+                    break
+                    
+                l += 1   
+                print("+++"+str(l)+"+++",rad,x_rad,axes[0])  
+                if l > 200 :
+                    break
 
-        rad = (abs(axes[0]*axes[1]*axes[2]))**(1./3.)
-        #print("________________________________________________________________")
-        print(x_rad,rad)
-        if( x_rad > 2.0*rvir ):
-            break
-    semmiaxes.append(np.array([rvir,rvir,rvir]))
-    semmiaxes = np.array(semmiaxes)
-    np.savetxt("../Plots/"+lvl+"/"+halo+"/"+"abc_"+lvl+"_"+halo+".txt",semmiaxes, delimiter = ',')
+            x_rad = 10.0**(logr_ini+(i+1)*logdelta)
+            if axes[0] > 0:
+                semmiaxes.append(np.copy(axes))
+                # Uses last result to improve convergence
+                axes[1:] = (x_rad/axes[0])*axes[1:]
+                axes[0] = x_rad
+                #m = 20
+                #if i%m == 0:
+                    #title = lvl+"_"+halo+"_"+str(i//m)+".png"
+                #a,b,c = axes
+                    #plotHalo(title,a,b,c,pos,lvl,halo)
+            else:
+                axes = np.ones(3,dtype = np.float)*x_rad
+                vecs = np.identity(3,dtype = np.float)
+
+            #rad = (abs(axes[0]*axes[1]*axes[2]))**(1./3.)
+            #print("________________________________________________________________")
+            #print(x_rad,rad)
+            if( x_rad > 1.0*rvir ):
+                break
+                
+        snapnum = snapnum-8
+        semmiaxes.append(np.array([rvir,rvir,rvir]))
+        semmiaxes = np.array(semmiaxes)
+        path = "../Plots/"+lvl+"/"+halo+"/"+"snap_"+str(snapnum)
+        # Tries to create directory
+        try:
+            os.makedirs(path)
+        except OSError:
+            print("Directory already exists")
+        np.savetxt(path+"/s"+str(snapnum)+".txt",semmiaxes, delimiter = ',')
+        np.save(path+"/info.npy",info)
+        
+
+    #np.savetxt("../Plots/"+lvl+"/"+halo+"/rand_sample/"+str(k)+".txt",semmiaxes, delimiter = ',')
+    #np.savetxt("../Plots/"+lvl+"/"+halo+"/"+"abc_"+lvl+"_"+halo+".txt",semmiaxes, delimiter = ',')
 '''
     if(len(semmiaxes) != 0):
         a,b,c = semmiaxes.T
@@ -95,4 +156,51 @@ for j in halonums:
         plt.savefig("../Plots/"+lvl+"/"+halo+"/"+"Axial_ratios_vs_R__"+lvl+"_"+halo+".png")
         plt.close()
 '''
+
+
+
+'''
+# Random sampling 10 times
+    for k in range(10):
+        print("Randomsample___halo:    "+str(k)+"    "+halo)
+        pos = np.random.permutation(pos1)
+        np.random.shuffle(pos)
+        np.random.shuffle(pos)
+        pos = pos[:int(len(pos)*1.0/8)]
+        print("Radius of the simulation:  "+str(rvir))
+        # Logspace for radius
+        logdelta = 1.0/50.0
+        logr_ini = -1
+        # Keeps semiaxes
+        semmiaxes = []
+        x_rad = 10.0**(logr_ini)
+        axes = np.ones(3,dtype = np.float)*x_rad
+        vecs = np.identity(3,dtype = np.float)
+        for i in range(1000):
+            inertia.get_shape(ct.c_void_p(pos.ctypes.data),ct.c_int(len(pos)), ct.c_void_p(vecs.ctypes.data), ct.c_void_p(axes.ctypes.data))
+            x_rad = 10.0**(logr_ini+(i+1)*logdelta)
+            if axes[0] > 0:
+                semmiaxes.append(np.copy(axes))
+                # Uses last result to improve convergence
+                axes[1:] = (x_rad/axes[0])*axes[1:]
+                axes[0] = x_rad
+                #m = 20
+                #if i%m == 0:
+                    #title = lvl+"_"+halo+"_"+str(i//m)+".png"
+                #a,b,c = axes
+                    #plotHalo(title,a,b,c,pos,lvl,halo)
+            else:
+                axes = np.ones(3,dtype = np.float)*x_rad
+                vecs = np.identity(3,dtype = np.float)
+
+            rad = (abs(axes[0]*axes[1]*axes[2]))**(1./3.)
+            #print("________________________________________________________________")
+            #print(x_rad,rad)
+            if( x_rad > 2.0*rvir ):
+                break
+                
+'''
+
+
+
 
