4fecb3e9acb98c817e243e96b77abdfee38cd03d
Jesus David Prada Gonzalez <jd.prada1760@uniandes.edu.co>
2018-07-17 01:17:52 +0200

    Linear model MCMC correlations

--- Code/effect_of_gas.py
+++ Code/effect_of_gas.py
@@ -1,3 +1,6 @@
+#import matplotlib
+#matplotlib.use('Agg')
+
 ########################################################################################################
 ##### THIS SCRITP IS FOR READING THE SUMARY OF IMPORTANT QUANTITIES FOR DM HALO SHAPE               ####
 ##### READS:                                                                                        ####
@@ -60,12 +63,26 @@ def main():
     # Important quantities (normalized)
     GasDiskRad = par['GasDiskRad']               # halfmass radius of gas
     StarDiskRad = par['StellarDiskRad']          # Same for stellar mass     
-    SFRD = par['SFR']/(StarDiskRad**2)           # Star formation ratio density
-    gas_den = par['MassGas']/(GasDiskRad**2)     # Mass densities
-    star_den = par['MassStars']/(StarDiskRad**2)
-    BH_den = par['MassBH']/(StarDiskRad**2)
-    bar_frac = (par['MassGas']+par['MassStars'])/par['MassDM'] # The baryonic fraction
+    gas_den = par['MassGas']#/(GasDiskRad**2)     # Mass densities
+    star_den = par['MassStars']#/(StarDiskRad**2)
+    BH_den = par['MassBH']#/(StarDiskRad**2)
+    bar_frac = (par['MassGas']+par['MassStars'])/(par['MassDM']+par['MassGas']+par['MassStars']) # The baryonic fraction
     
+    '''
+    import matplotlib.pyplot as plt
+    
+    plt.plot(StarDiskRad,bar_frac, 'ro')
+    plt.xlabel('Star Rad')
+    plt.ylabel('Baryonic Fraction')
+    plt.savefig('./pics/Baryons_plots/StarRad_barfrac.png')
+    plt.close()
+    
+    plt.plot(np.log10(star_den+gas_den),bar_frac, 'ro')
+    plt.xlabel('Star mass')
+    plt.ylabel('Baryonic Fraction')
+    plt.savefig('./pics/Baryons_plots/StarMass_barfrac.png')
+    plt.close()
+    '''
     # The disk directions 
     disk_vec = params.T[-3:].T
 
@@ -128,34 +145,173 @@ def main():
     # Defines Sphericity
     asph_MHD = asphericity(axesMHD) #shape: (nhalos,5)
     asph_DM  = asphericity(axesDM)
-    y = (asph_MHD - asph_DM)[:,0] # 0 -> only calculated at 0.125 Rvir
+    #y = (asph_MHD - 0*asph_DM)[:,1] # 0 -> only calculated at 0.125 Rvir
     
     #y1 = axesMHD[:,0,1]/axesMHD[:,0,0] # this is b/a MHD for all haloes
-    #y2 = axesMHD[:,0,2]/axesMHD[:,0,0] # c/a MHD
-    
-    # The X values (Parameters of the fit)
-    
+    y = ((axesMHD[:,0,2]/axesMHD[:,0,0]))**(-1) #- (axesDM[:,0,2]/axesDM[:,0,0])) # c/a MHD
+
     # Just to see if the increase is related to the unperturbed state of the halo
-    #x1 = axesDM[:,0,1]/axesDM[:,0,0] #b/a DM (q)
-    #x2 = axesDM[:,0,2]/axesDM[:,0,0] #c/a MHD (s)
-    X = np.array([GasDiskRad,StarDiskRad,np.log10(SFRD),np.log10(gas_den),np.log10(star_den),np.log10(BH_den),bar_frac]).T
+    x1 = axesDM[:,0,1]/axesDM[:,0,0] #b/a DM (q)
+    x2 = axesDM[:,0,2]/axesDM[:,0,0] #c/a MHD (s)
+    x1 = 1./x1
+    x2 = 1./x2
+    X = np.array([GasDiskRad,StarDiskRad,np.log10(gas_den),np.log10(star_den),np.log10(BH_den),bar_frac,x1,x2]).T
         
-    Xnames = ['Gas Radius','StarDisk Radius','SFR density','Gas density','Star density','BH density',
-                'Baryonic fraction']
-    Xlog = [0,0,1,1,1,1,0,0,0,0] # To know if scales are logarithm
+    Xnames = ['Gas Radius','StarDisk Radius','Gas density','Star density','BH density',
+                'Baryonic fraction','q','s']
+    Xlog = [0,0,1,1,1,0,0,0,0] # To know if scales are logarithm
+    
     
     # Halos to discard
-    i_discard = np.where(y<0.1)[0]
-    # i_discard = [10,13,16]
+    #i_discard = np.where(y<0.1)[0]
+    i_discard = [10]
     badX = X[i_discard]
     bady = y[i_discard]
     X = np.delete(X,i_discard,axis=0)
     y = np.delete(y,i_discard)
+    
+    #SVReg(X,(y),Xnames=Xnames, yname = 'Delta sphericity', Xlog = Xlog, badX=badX, bady=bady)
+      
+    # Mean values
+    meanX = np.mean(X, axis = 0)
+    # STD but with the signs of the coeficients
+    sigmaX = np.std(X, axis = 0)
+    
+    #X = ((X-meanX)/sigmaX)
+
+
+    MCMC(X,y,X_names=Xnames)
+
+    
+    
+    '''
+    print meanX[1]
+     
+    from scipy.optimize import curve_fit
+    def fun( x,a,b,c,n): 
+        return a/((x-b)**n)+c
+    popt,pcov = curve_fit(fun, X[:,1],X[:,-1])
         
-    SVReg(X,y,Xnames=Xnames, yname = 'Delta sphericity', Xlog = Xlog, badX=badX, bady=bady)
+    #scatter3d(X[:,-2], X[:,-1], y, y)    
+    plt.plot(X[:,1],X[:,-1],'ro')
+    xspc = np.linspace(min(X[:,1])-0.1, max(X[:,1])+0.1, 100)
+    print popt
+    plt.plot(xspc,fun(xspc,popt[0],popt[1],popt[2],popt[3]))   
+
+    plt.show()
+    '''
 
 
 
+def MCMC(X,y,X_names='',yname=''):
+    
+    # Basic imports
+    import emcee
+    
+    
+    
+    ##########################
+    ##### SETUP FUNCTIONS ####
+    ##########################
+    print X.shape
+    X = X.T
+    
+    # The model to fit parameters
+    # Asume linear model and power law for baryionic fraction
+    # Encode arguments of this function in theta
+    def model(theta,X):
+
+        theta = np.array(theta)
+        #print np.multiply(theta[:-1][:,np.newaxis],X)
+        val = np.sum(np.multiply(theta[:-1][:,np.newaxis],X),axis=0)+ theta[-1]
+        #print val
+        return val
+
+    # Define the ln of the likelihood.
+    # Just assume exponential of the squared error
+    def lnlike(theta, X, y, yerr=1):
+        
+        y_mod = model(theta,X)
+        inv_sigma2 = 1.0/(yerr**2) #+ model**2*np.exp(2*lnf))      -> This is more sophisticated
+        return -0.5*(np.sum((y-y_mod)**2*inv_sigma2 - np.log(inv_sigma2)))
+    
+    # The log of the prior probability
+    # All variables are free to choose (mainly slopes)
+    def lnprior(theta,X):
+        
+        #a0,a1,c = theta
+        if False : # keep power term positive
+            return -np.inf
+        return 0 # no restrictions
+    
+    # The log of the final probability of the model    
+    def lnprob(theta, X, y, yerr=1):
+        lp = lnprior(theta,X)
+        if not np.isfinite(lp): # Not needed but anyway jic
+            return -np.inf
+        return lp + lnlike(theta, X, y, yerr)
+    
+    # Now we find the maximum likelihood
+    import scipy.optimize as op
+    nll = lambda x,*t: model([t[i] for i in range(len(X)+1)],x)
+    print len(X.T),len(y)
+    x = np.transpose(X)
+    popt,pcov = op.curve_fit(nll,X,y,p0=0*np.ones(len(X)+1))
+    
+    import matplotlib.pyplot as plt     
+    #scatter3d(X[-2], X[-1], y, y)    
+    yopt = model(popt,X)
+    xlabs = X_names
+    for i in range(len(X)):
+        plt.plot(X[i],y,'bo')
+        plt.plot(X[i],yopt,'ro')
+        plt.ylabel(r'$ \frac{a}{c}$')
+        plt.xlabel(xlabs[i])       
+        plt.savefig('./pics/Baryons_plots/'+xlabs[i]+'.png')
+        plt.close()
+    #xspc = np.linspace(min(X[:,1])-0.1, max(X[:,1])+0.1, 100)
+    #print popt
+    #plt.plot(xspc,fun(xspc,popt[0],popt[1],popt[2],popt[3]))   
+
+    
+    
+    
+    
+    
+    print popt
+    # We initialize some walkers around the maximum likelihood
+    ndim, nwalkers = len(X)+1, 100
+    pos = [popt + (1e-6)*(1-2*np.random.randn(ndim)) for i in range(nwalkers)]
+    
+    # Set up the sampler
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=(X, y, 1.0))
+    
+    # Run nwalks 
+    sampler.run_mcmc(pos, 1000)
+    
+    # Retrieve chains after stabilization
+    samples = sampler.chain[:, 50:, :].reshape((-1, ndim))
+    
+    #print result
+    import corner    
+    fig = corner.corner(samples, labels= X_names+['$Intercept$'], truths = popt)
+    
+    results = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]),zip(*np.percentile(samples, [16, 50, 84],axis=0)))
+    
+    for el in zip(results,X_names+['Intercept']):
+        print el
+    
+    # Tries to create directory
+    path = './pics/Baryons_plots/'
+    try:
+        os.makedirs(path)
+    except OSError:
+        print("Directory already exists")
+    fig.savefig(path+'MCMC_mixed.png')
+    
+    
+
+    
 
 # Performs a Support vector regression according the given yparams and xparams
 # Plots important projections over the x space to see accuracy of fit.
@@ -164,16 +320,13 @@ def SVReg(X,y,Xnames='',yname='',Xlog = None, badX=None, bady=None):
     
     import matplotlib.pyplot as plt
     # Import scikit learn
-    from sklearn.svm import SVR
-    from sklearn.ensemble import RandomForestRegressor
-    from sklearn.linear_model import LassoCV
-    
-    # We apply SVR 
-    svr = SVR(kernel='linear', C = 1,epsilon = 0.075)
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.linear_model import Lasso
+
     # And random forest
-    rf = RandomForestRegressor(n_estimators=100)
+    rf = RandomForestClassifier(n_estimators=100)
     # And LassoCV
-    lasso = LassoCV()
+    lasso = Lasso(alpha =0.01)
     
     
     # Mean values
@@ -184,50 +337,86 @@ def SVReg(X,y,Xnames='',yname='',Xlog = None, badX=None, bady=None):
     X = ((X-meanX)/sigmaX)
     badX =  ((badX-meanX)/sigmaX)
     
+    # Labels for random forest
+    mid1 = np.percentile(y,33)
+    mid2 = np.percentile(y,66)
+    labels = (y>mid2).astype(np.int)
+    #labels+= (y>mid1).astype(np.int)
+    print(labels)
+    
     # Asuming array x is of size (npoints,mparams) and array y is of size (npoints)
-    svr.fit(X,y)
-    rf.fit(X,y)
+
     lasso.fit(X,y)
+    
+    rf.fit(X,labels)
+    
+    fit_labs = rf.predict(X)
     
     # Linspaces for each variable
     linspcX = np.array([np.linspace(min(x)-0.01,max(x)+0.01,100) for x in X.T])
     #print Xs
     
+    y_data_lasso = lasso.predict(X) 
+    
     for i in range(len(Xnames)):
         # Names and slopes
         print Xnames[i],
         print '\n',
-        print 'Mean',svr.coef_[0,i]
-        print 'Feature Importance',rf.feature_importances_[i],'\n'
+        print 'Slope',lasso.coef_[i]
+        print 'Feature Importance',rf.feature_importances_[i]
+        print 'Feature correlation',np.corrcoef(X[:,i],y)[0,1],'\n'
         
-        # Plot dataset projection over variable i
+        #plt.plot(X[:,1],X[:,i], 'bo')
+        
+        # Plot dataset projection over variable 
+        
         plt.plot(X[:,i],y,'bo')
+        
         plt.plot(badX[:,i],bady,'ro')
         
+        #plt.hlines(mid, min(linspcX[i]), max(linspcX[i]), linestyle='-.')
         # We produce a generic space of with the linspace of the projected variable
         # and the mean in the other variables.
         # We also take the uppper and lower bounds in the other variables
         
-        X_proj_mean = np.zeros((100,len(meanX)))
-        X_proj_mean[:,i] = linspcX[i]
+        #X_proj_mean = np.zeros((100,len(meanX)))
+        #X_proj_mean[:,i] = linspcX[i]
 
-        yproj_svr = svr.predict(X_proj_mean)
-        yproj_rf = rf.predict(X_proj_mean)
-        yproj_lasso = lasso.predict(X_proj_mean)
+        #yproj_svr = svr.predict(X_proj_mean)
+        #yproj_rf = rf.predict(X_proj_mean)
+        #yproj_lasso = lasso.predict(X_proj_mean)
         
-        plt.plot(linspcX[i],yproj_svr,'black',linewidth=1.2,label='SVR') # Mean
-        plt.plot(linspcX[i],yproj_rf,'g',linewidth=1.2,label='Random Forest') # Mean
-        plt.plot(linspcX[i],yproj_lasso,'r',linewidth=1.2,label='Lasso') # Mean
+        #plt.plot(linspcX[i],yproj_svr,'black',linewidth=2,label='SVR') # Mean
+        #plt.plot(X[:,i],y_data_svr,'g*',label='SVR_data') # Mean
+        
+        
+        
+        #plt.plot(linspcX[i],yproj_lasso,'r',linewidth=2,label='Lasso') # Mean
+        #plt.plot(X[:,i],y_data_lasso,'m^',linewidth=2,label='Lasso_data') # Mean
 
         plt.legend(loc=0)
+        
         plt.xlabel(Xnames[i])
-        plt.show()
-        plt.close() 
-        
-        
-      
-    print svr.coef_,svr.intercept_          
+        #plt.show()
+        plt.close()
+    
+    print "FIN"     
 
+
+def scatter3d(x,y,z, cs, colorsMap='jet'):
+    import matplotlib
+    import matplotlib.pyplot as plt
+    import matplotlib.cm as cmx
+    from mpl_toolkits.mplot3d import Axes3D
+    cm = plt.get_cmap(colorsMap)
+    cNorm = matplotlib.colors.Normalize(vmin=min(cs), vmax=max(cs))
+    scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=cm)
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.scatter(x, y, z, c=scalarMap.to_rgba(cs))
+    scalarMap.set_array(cs)
+    fig.colorbar(scalarMap)
+    plt.show()
     
 def simple_plot(x,y,xlabel,ylabel,title, mode = 'scatter', logx = False, folder = ''):
     
@@ -278,7 +467,7 @@ def read_csv(filename, skip_header = 1):
 # Triaxialities T = (1-(b/a)^2)/(1-(c/a)^2) 
 # (T = 0 --> Oblate ) (T = 1 --> Prolate ) (T = 0.5 --> Max Triax )
 def get_T(axes):
-   return (axes[:,:,0]**2-axes[:,:,1]**2)/(axes[:,:,0]**2-axes[:,:,2]**2)               
+    return (axes[:,:,0]**2-axes[:,:,1]**2)/(axes[:,:,0]**2-axes[:,:,2]**2)               
         
 # Calculates a measure of asphericity
 # Returns the 1 minus actual distance from the point in the plane (c/a,b/a) to the point (1,1) in the triaxiality plane
@@ -411,16 +600,16 @@ def polar_graphs(pdf,vecs,disk_vec,radii,title, radii_vals = None, mode = 'all_i
             midp = (np.pi/180.)*midp
             majp = (np.pi/180.)*majp
             
-            print "Minor: "
-            print vecs[0,[1,0,2],:].dot(vecs[i,2,:])
-            print to_Cartesians(mint,minp)
-            print "Mid: "
-            print vecs[0,[1,0,2],:].dot(vecs[i,1,:])
-            print to_Cartesians(midt,midp)
-            print "Major: "
-            print vecs[0,[1,0,2],:].dot(vecs[i,0,:])
-            print to_Cartesians(majt,majp)
-            print "____________________________________________________"
+            #print "Minor: "
+            #print vecs[0,[1,0,2],:].dot(vecs[i,2,:])
+            #print to_Cartesians(mint,minp)
+            #print "Mid: "
+            #print vecs[0,[1,0,2],:].dot(vecs[i,1,:])
+            #print to_Cartesians(midt,midp)
+            #print "Major: "
+            #print vecs[0,[1,0,2],:].dot(vecs[i,0,:])
+            #print to_Cartesians(majt,majp)
+            #print "____________________________________________________"
  
             
             #print mint, ec[int(mint>90)], int(mint>90), int(mint<90), ec
